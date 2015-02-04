582552f180d2b9d16ae1a0a124caf3cfd5214994
MarshalJoe <joseph.charles.marshall@gmail.com>
2015-02-04 13:58:34 -0600

    Adds support for scheduling. Runs every 55th minute.

--- start.js
+++ start.js
@@ -1,3 +1,11 @@
 var Bot = require("./newBot.js")
+var schedule = require('node-schedule');
+ 
+var rule = new schedule.RecurrenceRule();
+rule.minute = 55;
+ 
+var j = schedule.scheduleJob(rule, function(){
+    console.log('-------- New Job Started -----------');
+    Bot.runBot();
+});
 
-Bot.runBot();
