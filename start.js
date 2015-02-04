404b2a0b156db742569dbec068141134207fdab3
MarshalJoe <joseph.charles.marshall@gmail.com>
2015-02-04 16:07:02 -0600

    Adds schedule for bot to post every day at 5pm

--- start.js
+++ start.js
@@ -1,9 +1,13 @@
 var Bot = require("./newBot.js")
 var schedule = require('node-schedule');
- 
+
+// Create a rule so that the Bot posts every day at 5pm 
 var rule = new schedule.RecurrenceRule();
-rule.minute = 55;
- 
+rule.dayOfWeek = [new schedule.Range(0, 6)];
+rule.hour = 17;
+rule.minute = 0;
+
+// Schedule Bot job 
 var j = schedule.scheduleJob(rule, function(){
     Bot.runBot();
 });
