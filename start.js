d0844d29962ed1355df5209b60b6490d5f6ca033
MarshalJoe <joseph.charles.marshall@gmail.com>
2015-02-17 15:09:04 -0600

    Adds greeting response

--- start.js
+++ start.js
@@ -1,17 +1,23 @@
 var Bot = require("./Bot.js")
 var schedule = require('node-schedule');
 var util = require('util');
+var Tweet = require('./tweet.js');
 
 // Create a rule so that the Bot posts every day at 5pm 
 var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [new schedule.Range(0, 6)];
-rule.hour = 17;
-rule.minute = 0;
+rule.hour = 15;
+rule.minute = 05;
 
 // Schedule Bot job 
 var j = schedule.scheduleJob(rule, function(){
     Bot.runBot();
 });
+
+// Start tweet streaming
+Tweet.streamTweets();
+
+console.log("Listening for tweets...");
 
 // Check Memory Footprint
 // console.log("Memory Usage");
--- tweet.js
+++ tweet.js
@@ -7,5 +7,34 @@ module.exports = {
 		RealtorBot.post('statuses/update', { status: content }, function(err, data, response) {
 			//console.log('Message sent: ' + data.text);
 		});
+	},
+	streamTweets: function () {
+		var Twit = require('twit');
+
+		var T = new Twit(require('./config.js'));;
+		var stream = T.stream('statuses/filter', { track: '@ABoR_MLSstats' })
+
+		stream.on('tweet', function (tweet) {
+  			// greetings to respond to (in order to prevent spamming mentions)
+  			var greetings = ["Hi", "hi", "Hello", "hello", "Hey", "hey"];
+
+			for (index in greetings) {
+			    if (tweet.text.indexOf(greetings[index]) !=-1) {
+			        var content = greetings[index] + " @" + tweet.user.screen_name;
+			    	T.post('statuses/update', { status: content }, function(err, data, response) {
+						if (err) {
+							console.log(err);
+							return
+						} else {
+							console.log(data.text);
+							return
+						}	
+					});
+			    }
+			}
+
+			console.log(tweet.text);
+
+		})
 	}
 }
