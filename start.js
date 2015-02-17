8338173fe69b758a6842a852f7902165f82e1127
MarshalJoe <joseph.charles.marshall@gmail.com>
2015-02-17 15:13:28 -0600

    Correctly changes back stats tweet schedule

--- start.js
+++ start.js
@@ -6,8 +6,8 @@ var Tweet = require('./tweet.js');
 // Create a rule so that the Bot posts every day at 5pm 
 var rule = new schedule.RecurrenceRule();
 rule.dayOfWeek = [new schedule.Range(0, 6)];
-rule.hour = 15;
-rule.minute = 05;
+rule.hour = 17;
+rule.minute = 0;
 
 // Schedule Bot job 
 var j = schedule.scheduleJob(rule, function(){
